8c53619238fb4fb697c4cab0129eb9a5a644636e
minhquangpham <pham@slurm.lab-ia.local>
2019-08-01 17:30:28 +0200

    remove config

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=20
 #SBATCH --output=decode.log.out
@@ -10,4 +10,4 @@ export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64/:/users/limsi_nmt/minhquang/cu
 
 which python
 python -c 'import tensorflow; print("tensorflow OK"); import opennmt; print("opennmt OK")'
-python -u inference.py --config_file /mnt/beegfs/home/pham/multi-domain-nmt/meta_sparse/configs/sparse_src_masking_70.yml --eval_step 200000
+python -u inference.py --config_file /mnt/beegfs/home/pham/multi-domain-nmt/LDR/configs/sparse_src_masking_73.yml --eval_step 105000
